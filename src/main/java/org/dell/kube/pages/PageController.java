be8bb515f68a89c4a7f67efc01f6463284dbf834
rajsharmabannu <rajsharmabannu@gmail.com>
2020-09-02 09:25:50 +0100

    added logging

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -2,11 +2,14 @@ package org.dell.kube.pages;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 @RestController
 @RequestMapping("/pages")
 public class PageController {
     private IPageRepository pageRepository;
+    Logger logger =(Logger)LoggerFactory.getLogger(this.getClass());
     public PageController(IPageRepository pageRepository)
     {
         this.pageRepository = pageRepository;
@@ -16,29 +19,53 @@ public class PageController {
         Page newPage= pageRepository.create(page);
         return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
     }
+
     @GetMapping("{id}")
     public ResponseEntity<Page> read(@PathVariable long id) {
+
+        logger.info("READ-INFO:Fetching page with id = " + id);
+        logger.debug("READ-DEBUG:Fetching page with id = " + id);
+
         Page page = pageRepository.read(id);
         if(page!=null)
             return new ResponseEntity<Page>(page, HttpStatus.OK);
-        else
+        else {
+            logger.error("READ-ERROR:Could not find page with id = " + id);
             return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
+
     @GetMapping
     public ResponseEntity<List<Page>> list() {
+        logger.info("READ-INFO:Fetching list of pages. ");
+        logger.debug("READ-DEBUG:Fetching list of pages. ");
         List<Page> pages= pageRepository.list();
-        return new ResponseEntity<List<Page>>(pages, HttpStatus.OK);
+        if(pages != null)
+            return new ResponseEntity<List<Page>>(pages, HttpStatus.OK);
+        else {
+            logger.error("READ-ERROR:Could not find list of pages. ");
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
     @PutMapping("{id}")
     public ResponseEntity<Page> update(@RequestBody Page page, @PathVariable long id) {
+
+        logger.info("READ-INFO: Updating page with id = " + id);
+        logger.debug("READ-DEBUG: Updating page with id = " + id);
+
         Page updatedPage= pageRepository.update(page,id);
         if(updatedPage!=null)
             return new ResponseEntity<Page>(updatedPage, HttpStatus.OK);
         else
+            logger.error("READ-ERROR:Could not update page with id = " + id);
             return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
     @DeleteMapping("{id}")
     public ResponseEntity delete(@PathVariable long id) {
+
+        logger.info("READ-INFO: Deleting page with id = " + id);
+        logger.debug("READ-DEBUG: Deleting page with id = " + id);
+
         pageRepository.delete(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
